3bd8730d56fb3074856e7657a6ac928431e4df44
Vladislav <V.kirbaba@gmail.com>
2015-07-03 13:09:16 +0300

    blog

--- view/blog/blog_view.php
+++ view/blog/blog_view.php
@@ -1,4 +1,8 @@
-<div class = "blog_wr"><span>Блог</span><a class = "blog_all" href = "{link_blog_all}">Смотреть все записи</a>
-<div class = "post_wr">{posts}</div>
-<div class = "get_mail"><a href = "#">Получать обновления на почту</a></div>
-</div>
+<srction class="blog">
+	<div class="container">
+		<div class = "blog_wr"><span>Блог</span><a class = "blog_all" href = "{link_blog_all}">Смотреть все записи</a>
+			<div class = "post_wr">{posts}</div>
+			<div class = "get_mail"><a class="takeNew" href = "#nowhere">Получать обновления на почту</a></div>
+		</div>
+	</div>
+</srction>
