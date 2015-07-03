49e4d57c2227c28671685b133d0ff1b4b2678aa8
Vladislav <V.kirbaba@gmail.com>
2015-07-03 16:07:24 +0300

    full page

--- view/reviews/reviews_block_view.php
+++ view/reviews/reviews_block_view.php
@@ -1,17 +1,25 @@
-<div class = "reviews_wr">
-    <h1 class = "blockTitle">Отзывы</h1><span>Смотреть {kol_reviews} {ending}</span>
-    <div class = "reviews">
-        <div class = "textreviews">
-            {text}
-        </div>
-        <div class = "fioreviews">
-            {fio}
-        </div>
-        <div class = "namereviews">
-            {name}
-        </div>
-        <div class = "linkreviews">
-           <a href = "{link}"> {link}</a>
+<section class = "reviews_wr">
+    <div class="container">
+        <div class="reviewsBlock">
+            <div class="reviewsBlockTitle">
+                <h1 class = "blockTitle">Отзывы <a class = "reviews_all" href = "{link_blog_all}">Смотреть {kol_reviews} {ending}</a></h1>
+            </div>
+            
+            <div class = "reviews">
+                <div class = "textreviews">
+                    {text}
+                </div>
+                <div class = "fioreviews">
+                    {fio}
+                </div>
+                <div class = "namereviews">
+                    {name}
+                </div>
+                <div class = "linkreviews">
+                   <a href = "{link}"> {link}</a>
+                </div>
+                <a href="#nowhere" class="circArrow"></a>
+            </div>
         </div>
     </div>
-</div>
+</section>
