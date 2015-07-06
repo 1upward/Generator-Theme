31adc6f1ee4514a51b3483baeda425657475b429
Vladislav <V.kirbaba@gmail.com>
2015-07-06 16:45:16 +0300

    pixel to pixel

--- view/add_work_view.php
+++ view/add_work_view.php
@@ -1,9 +1,12 @@
+
 <form action="/wp-admin/admin.php?page=gen_theme" method="post">
-    <p><b>Выберите изображение:</b><br>
-    <p><img class="custom_media_image" src="" alt="" style="width: 80px;"></p>
-    <p><button class="custom_media_upload">Загрузить</button></p>
-    <p><input id="image" class="custom_media_url" type="hidden" name="attachment_url" value=""></p>
-    <p>Введите название <input type="text" name="name_rab" id="" /></p>
-    <p>Введите ссылку <input type="text" name="link_rab" id="" /></p>
-    <p><input type="submit" name="" id="" value="Сохранить"/></p>
+	<div class="container">
+	    <p><b>Выберите изображение:</b><br>
+	    <p><img class="custom_media_image" src="" alt="" style="width: 80px;"></p>
+	    <p><button class="custom_media_upload">Загрузить</button></p>
+	    <p><input id="image" class="custom_media_url" type="hidden" name="attachment_url" value=""></p>
+	    <p>Введите название <input type="text" name="name_rab" id="" /></p>
+	    <p>Введите ссылку <input type="text" name="link_rab" id="" /></p>
+	    <p><input type="submit" name="" id="" value="Сохранить"/></p>
+    </div>
 </form>
--- view/work_box_view.php
+++ view/work_box_view.php
@@ -1,9 +1,12 @@
-<div style = "float: left;margin-right: 10px;">
+<div class="container">
+<div style = "float: left; min-height: 200px; margin: 25px 10px; padding: 10px; background-color: #fff; border-radius: 10px;">
     <a href="{link}" class="jobItem">
                     <span class="jobImg">
-                        <img src="{images}" alt="">
+                        <img style="max-width: 170px" src="{images}" alt="">
                     </span>
         <p>{name}</p>
     </a>
+
     <a href="/wp-admin/admin.php?page=gen_theme&action=del&id={id}">Удалить</a>
+</div>
 </div>
