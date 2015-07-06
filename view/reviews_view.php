76dc839b2004be74114e1de84603389164c12f0b
Vladislav <V.kirbaba@gmail.com>
2015-07-06 16:45:58 +0300

    admin panel style

--- view/reviews_view.php
+++ view/reviews_view.php
@@ -1,7 +1,7 @@
 <h1>Отзывы</h1>
 <a href = "/wp-admin/admin.php?page=gen_reviews&action=add_reviews">Добавить отзыв</a>
 
-<table width = "100%" style = "text-align: center;">
+<table class="admin_reveiws" border=1 width = "100%" style = "margin-top: 15px;text-align: center; padding: 10px 20px; background-color: #fff; border-radius: 10px; border: 3px;">
     <tr>
         <th></th>
         <th>Отзыв</th>
