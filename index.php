a68ee18587b48ad4e29ab36f8fa5d10cd051be9a
Vladislav <V.kirbaba@gmail.com>
2015-07-09 13:12:02 +0300

    правки

--- index.php
+++ index.php
@@ -434,48 +434,51 @@
     <?php echo do_shortcode("[blog]");?>
 
    	<footer>
-   		<div class="topLine">			
-			<div class="topLine-left">				
-				<p>Наши проекты:</p>
-				<a href="http://promo.itpanda.ru">promo.itpanda.ru</a>
-				<a href="http://shop.itpanda.ru">shop.itpanda.ru</a>
+   		<div class="topLineBorder">
+	   		<div class="topLine">		   					
+					<div class="topLine-left">				
+						<p>Наши проекты:</p>
+						<a href="http://promo.itpanda.ru">promo.itpanda.ru</a>
+						<a href="http://shop.itpanda.ru">shop.itpanda.ru</a>
+					</div>
+					<div class="topLine-right">
+						<ul>
+							<li><a href="#">Новости</a></li>
+							<li><a href="#">Вакансии</a></li>
+							<li><a href="#">Генератор продаж</a></li>
+						</ul>
+					</div>	
+					
 			</div>
-			<div class="topLine-right">
-				<ul>
-					<li><a href="#">Новости</a></li>
-					<li><a href="#">Вакансии</a></li>
-					<li><a href="#">Генератор продаж</a></li>
-				</ul>
-			</div>			
 		</div>
-
-		<div class="footerContent">
-			<div class="footContacts">
-				<h3>© «IT PANDA», 2012-2015</h3>
-				<p>Екатеринбург	&nbsp +7 (343) 226-01-36</p>
-				<p>Челябинск &nbsp	+7 (351) 216-03-24</p>
-				<p>Москва &nbsp	+7 (499) 704-62-76</p>
-				<p>Тюмень	&nbsp +7 (3452) 23-83-14</p>
-				<p>Вся Россия &nbsp	8 (800) 333-6990</p>
-				<br>
-				<a href="#nowhere">Карта проезда</a><a href="mailto=sales@itpanda.ru">sales@itpanda.ru</a>
-			</div>
-			<div class="footSlogan">
-				<h3>Создание продающих сайтов в Екатеринбурге и по России</h3>
-				<h3>Продвижение сайтов в IT PANDA - itpanda-promo.ru</h3>
-				<div class="footSloganBut">
-					<a data-toggle="modal" href="#request">Запросить портфолио</a>
-					<p>Мы обязательно ответим</p>
+			<div class="footerContent">
+				<div class="footContacts">
+					<h3>© «IT PANDA», 2012-2015</h3>
+					<p>Екатеринбург	&nbsp +7 (343) 226-01-36</p>
+					<p>Челябинск &nbsp	+7 (351) 216-03-24</p>
+					<p>Москва &nbsp	+7 (499) 704-62-76</p>
+					<p>Тюмень	&nbsp +7 (3452) 23-83-14</p>
+					<p>Вся Россия &nbsp	8 (800) 333-6990</p>
+					<br>
+					<a href="#nowhere">Карта проезда</a><a href="mailto=sales@itpanda.ru">sales@itpanda.ru</a>
 				</div>
-			</div>
-			<div class="footSoc">
-				<h3>Мы в соц.сетях</h3>
-				<ul class="footSocList">
-					<li><a href="#"><i class="t"></i></a></li>
-					<li><a href="#"><i class="fb"></i></a></li>		            
-		            <li><a href="#"><i class="vk"></i></a></li>		            
-        		</ul>
-			</div>
+				<div class="footSlogan">
+					<h3>Создание продающих сайтов в Екатеринбурге и по России</h3>
+					<h3>Продвижение сайтов в IT PANDA - itpanda-promo.ru</h3>
+					<div class="footSloganBut">
+						<a data-toggle="modal" href="#request">Запросить портфолио</a>
+						<p>Мы обязательно ответим</p>
+					</div>
+				</div>
+				<div class="footSoc">
+					<h3>Мы в соц.сетях</h3>
+					<ul class="footSocList">
+						<li><a href="#"><i class="t"></i></a></li>
+						<li><a href="#"><i class="fb"></i></a></li>		            
+			            <li><a href="#"><i class="vk"></i></a></li>		            
+	        		</ul>
+				</div>
+			
 		</div>
    	</footer>
 
